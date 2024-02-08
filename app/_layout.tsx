4be0444c6149881c0e6de8e3db5013992a4ac93c
Thom Lamb <thom@codebusters.ca>
2024-02-08 10:38:36 -0500

    Expo router auth works

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Slot } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
+import { SessionProvider } from './ctx';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -46,11 +47,8 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+   <SessionProvider>
+      <Slot />
+   </SessionProvider>
   );
 }
